64c8b5e2e181077465e4d59c9bf0ff42f3bda190
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-14 22:07:51 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default ({
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    "^.+\\.(js|jsx)$": "babel-jest",
-  }
+  testEnvironment: 'node'
 });
