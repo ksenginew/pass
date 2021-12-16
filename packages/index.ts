843f22271d7d18d495c33ea5dbafbeb98e06fb3d
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-16 08:09:32 +0530

    Create index.ts

---
+++ packages/index.ts
@@ -0,0 +1,11 @@
+import { compileString } from 'sass'
+import type { StringOptions } from 'sass'
+import { css as pass } from '@pass-lang'
+
+export * from 'sass'
+
+export let scss = (options?: StringOptions) => (...args) => compileString(pass(...args), options)
+
+export let sass = (options: StringOptions = {}) => scss({ style: 'indented', ...options})
+
+export default scss();
