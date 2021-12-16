c1890a7e0da6f3abc3993e5cb8d3fb1da1375a27
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-16 12:25:46 +0530

    Update index.ts

--- packages/sass/index.ts
+++ packages/sass/index.ts
@@ -10,6 +10,6 @@ export * from 'sass'
 
 export let scss = (options?: StringOptions<'sync'>) => (strings: TemplateStringsArray, ...args: any[]) => compileString(pass(strings, ...args), options)
 
-export let sass = (options: StringOptions<'sync'> = {}) => scss({ style: 'indented', ...options})
+export let sass = (options: StringOptions<'sync'> = {}) => scss({ syntax: 'indented', ...options})
 
 export default scss();
