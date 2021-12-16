83a2285411102ad7290ce66e36763142c8bfbb14
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-16 18:38:38 +0530

    commit

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = ({
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@pass/(.*?)$': '<rootDir>/packages/$1/src',
+    pass: '<rootDir>/packages/pass/src'
+  },
+  rootDir: __dirname,
 });
