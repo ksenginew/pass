e37bf571ec361df611c6b4eb91bce82794fb6a2f
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-16 12:14:55 +0530

    Update index.ts

--- packages/sass/index.ts
+++ packages/sass/index.ts
@@ -5,9 +5,10 @@ import type { StringOptions } from 'sass'
 // @ts-ignore
 import { css as pass } from 'pass-lang'
 
+// @ts-ignore
 export * from 'sass'
 
-export let scss = (options?: StringOptions) => ((...args) => compileString(pass(...args), options)) as typeof pass
+export let scss = (options?: StringOptions) => (strings: TemplateStringsArray, ...args: any[]) => compileString(pass(strings, ...args), options)
 
 export let sass = (options: StringOptions = {}) => scss({ style: 'indented', ...options})
 
