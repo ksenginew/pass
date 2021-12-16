c658db3856dea74848fcfb4f5f98d6f1b6bdb6d7
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-16 12:50:02 +0530

    Update index.ts

--- packages/sass/index.ts
+++ packages/sass/index.ts
@@ -12,7 +12,9 @@ export let scss =
   (options?: StringOptions<"sync">) =>
   (strings: TemplateStringsArray, ...args: any[]) => {
     let result = compileString(pass(strings, ...args), options);
-    return Object.assign(result.css, result);
+    let css = result.css
+    css.sourceMap = result.sourceMap
+    return 
   };
 
 export let sass = (options: StringOptions<"sync"> = {}) =>
