6848e035b28c45a1dc45920950cf42867b05a1a8
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-19 17:23:22 +0530

    Update docusaurus.config.js

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
         title: 'Pass',
         logo: {
           alt: 'Pass logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
