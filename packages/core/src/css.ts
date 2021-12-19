d7faf486edb882ef72b176f8b0462f6378817a31
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-19 11:48:20 +0530

    Update css.ts

--- packages/core/src/css.ts
+++ packages/core/src/css.ts
@@ -1,14 +1,13 @@
 import { stringify } from './stringify';
 
-export { stringify } from './stringify'
-
 export let css = (strings: TemplateStringsArray, ...args: any[]) =>
     strings.reduce(
         (p, c, index) => {
             let value = args[index]
             if (typeof value == 'object')
                 value = stringify(value)
-            return p + c + (value ?? '')
+            // `value == undefined` can detect null too.
+            return p + c + (value == undefined ? '' : value);
         },
         ""
     );
