0a1a9c23d18905feeeb7075e269d0ef9252fd50a
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-19 12:01:51 +0530

    Create stringify.test.ts

---
+++ packages/core/src/stringify.test.ts
@@ -0,0 +1,32 @@
+import { stringify } from "./stringify";
+
+describe("stringify", (stringify) => {
+  test("empty object", () => {
+    expect(stringify({})).toMatchInlineSnapshot(`""`);
+  });
+
+  test("simple object", () => {
+    expect(stringify({ color: 'red' })).toMatchInlineSnapshot(`""`);
+  });
+
+  test("nested object", () => {
+    expect(stringify({
+      color: 'red',
+      ':hover': {
+        color: 'blue'
+      }
+    })).toMatchInlineSnapshot(`""`);
+  });
+
+  test("undefined value", () => {
+    expect(stringify({ color: undefined })).toMatchInlineSnapshot(`""`);
+  });
+
+  test("null value", () => {
+    expect(stringify({ color: null })).toMatchInlineSnapshot(`""`);
+  });
+
+  test("array value", () => {
+    expect(stringify({ color: ['red', 'blue'] })).toMatchInlineSnapshot(`""`);
+  });
+});
