92e974f5e74c5236282d92473cefcb28ed857872
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-25 08:54:35 +0530

    Update docusaurus.config.js

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -49,7 +49,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'tutorial/index',
+            docId: 'tutorial',
             position: 'left',
             label: 'Tutorial',
           },
@@ -69,7 +69,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/tutorial',
+                to: '/docs/tutorial/',
               },
             ],
           },
