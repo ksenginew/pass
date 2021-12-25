f49821eabd7f36f3de4b4c5eced063b687a5757e
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-25 15:07:35 +0530

    Update index.js

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/tutorial">
+            to="/docs/tutorial/introduction">
             Tutorial
           </Link>
         </div>
