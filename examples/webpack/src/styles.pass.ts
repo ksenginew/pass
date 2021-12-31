60f0938afedb6b0c5a49d5a5ce9232e69cbee5c3
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-31 16:41:55 +0530

    Update and rename styles.pass.js to styles.pass.ts

--- examples/webpack/src/styles.pass.ts
+++ examples/webpack/src/styles.pass.ts
@@ -2,9 +2,9 @@
 import { css } from '@passlang/core'
 
 // define variables for the primary colors
-let primary_1 = '#a2b9bc'
-let primary_2 = '#b2ad7f'
-let primary_3 = '#878f99'
+let primary_1:string = '#a2b9bc'
+let primary_2:string = '#b2ad7f'
+let primary_3:string = '#878f99'
 
 // use the variables and export CSS
 export default css`
