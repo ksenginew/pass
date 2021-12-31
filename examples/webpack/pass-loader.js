981044d4ca1831f7c4ac89c96574a37091d5c9c6
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-31 16:14:46 +0530

    Update pass-loader.js

--- examples/webpack/pass-loader.js
+++ examples/webpack/pass-loader.js
@@ -2,6 +2,5 @@ exports.pitch = async function (remaining) {
   const result = await this.importModule(
     this.resourcePath + '.webpack[javascript/auto]' + '!=!' + remaining
   );
-  this.clearDependencies();
   return result.default || result;
 };
