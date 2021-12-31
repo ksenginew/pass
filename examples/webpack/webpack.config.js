1a44785e7388daa4dc054202c072b5f70e63e499
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-31 16:56:22 +0530

    Update webpack.config.js

--- examples/webpack/webpack.config.js
+++ examples/webpack/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, '__dist__'),
     filename: 'bundle.js',
